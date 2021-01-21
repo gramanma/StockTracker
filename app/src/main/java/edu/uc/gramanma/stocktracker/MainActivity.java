c7cd58d2402512c27133675c24cc8c0720f4c1d3
gramanma <55257671+gramanma@users.noreply.github.com>
2021-01-21 18:19:28 -0500

    Initial commit

---
+++ app/src/main/java/edu/uc/gramanma/stocktracker/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.uc.gramanma.stocktracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.uc.gramanma.stocktracker.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/edu/uc/gramanma/stocktracker/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package edu.uc.gramanma.stocktracker.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
