eabb8081a92c1102895c9805b622e7f00f904263
gramanma <55257671+gramanma@users.noreply.github.com>
2021-04-05 17:45:19 -0400

    Update MainFragment.kt

--- app/src/main/java/edu/uc/coffeens/stocktracker/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/coffeens/stocktracker/ui/main/MainFragment.kt
@@ -85,8 +85,10 @@ class MainFragment : Fragment() {
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == AUTH_REQUEST_CODE) {
-            user = FirebaseAuth.getInstance().currentUser
+        if (resultCode == RESULT_OK) {
+            if (requestCode == AUTH_REQUEST_CODE) {
+                user = FirebaseAuth.getInstance().currentUser
+            }
         }
     }
 
